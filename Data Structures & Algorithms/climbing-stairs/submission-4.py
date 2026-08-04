5399057a73cadf7d4a8a7330dd089381a94c7935
priyankaonta <68300973+priyankaonta@users.noreply.github.com>
2026-08-04 14:51:21 -0400

    Add: climbing-stairs - submission-4

---
+++ Data Structures & Algorithms/climbing-stairs/submission-4.py
@@ -0,0 +1,10 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        DP = []
+        DP.insert(0,1)
+        DP.insert(1,1)
+        for i in range(2,n+1):
+            DP.append(DP[i-1] + DP[i-2])
+        return DP[n]
+        
+        
